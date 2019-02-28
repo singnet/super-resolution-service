dcdc78e6fb18813e09720ecdf3b3d4fe40548ad9
Ramon Duraes <ramongduraes@gmail.com>
2019-02-28 08:05:30 -0300

    Removing png conversion

--- service/increase_resolution.py
+++ service/increase_resolution.py
@@ -130,7 +130,7 @@ if __name__ == '__main__':
             ssim_mean = 0
         try:
             for iid, data in enumerate(data_loader):
-                log.debug("Performing SR!")
+                log.debug("Performing SR! IID: {} , DATA : {}".format(iid, data))
                 tic = time.time()
                 input = data['input']
                 if not input_args.cpu:
